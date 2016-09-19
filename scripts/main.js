4c13a70cd33294e874412df41005995775c225d5
dpw1 <diego.boarutto.fortes@gmail.com>
2016-09-19 18:48:31 +0100

    First commit with two modules ready

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+/*    $(function () {
+        $("header").load("modules/header.html");
+        $(".hero").load("modules/hero.html");
+        //$("footer").load("modules/footer.html");
+    });*/
+});
